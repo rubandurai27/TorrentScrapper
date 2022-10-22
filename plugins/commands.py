cc5235ca1b44b8a82eb4d8e662a715124f8af70c
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2022-10-22 10:52:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -3,17 +3,17 @@ from pyrogram import Client, filters
 txt = '''
 Commands:
 
-/listmv [query] - to search movies in TamilMV
+/tmv [query] - to search movies in TamilMV
 
-/listbl [query] - to search movies in Tamilblasters
+/tbl [query] - to search movies in Tamilblasters
 
 /post [tamilmv link] - makes a post of a given link
 
-/latest - to get screenshot of latest movies in TamilMV and Tamilblasters
+/webshot - to get screenshot of latest movies in TamilMV and Tamilblasters
 
 Bot Usage:
 
-Send any 1tamilmv.com or tamilblasters.com link to scrap torrents
+Send any 1tamilmv.com or tamilblasters.com link to scrap .
 '''
 
 @Client.on_message(filters.command('start'))
